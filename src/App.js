a3076f149748b7fe26416c970ddd7979e62af15d
bro <gudgudgud714@naver.com>
2022-05-25 10:41:08 +0900

    1.1 useInput

--- src/App.js
+++ src/App.js
@@ -1,52 +1,27 @@
 import React, { useState } from "react";
 
-// 함수형 컴포넌트
-const App = () => {
-  const [item, setItem] = useState(1);
-  const plus = () => setItem(item + 1);
-  const minus = () => setItem(item - 1);
-
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox {item}</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <button onClick={plus}>+</button>
-      <button onClick={minus}>-</button>
-    </div>
-  );
+const useInput = (initialValue) => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = (e) => {
+    console.log(e.target);
+  }
+  return { value, onChange };
 }
 
-// 클래스형 컴포넌트
-class ClassApp extends React.Component {
-  state = {
-    item: 1
-  }
-  render() {
-    const { item } = this.state;
-    return (
-      <div className="App">
-        <h1>Hello CodeSandbox Class {item}</h1>
-        <h2>Start editing to see some magic happen!</h2>
-        <button onClick={this.plus}>+</button>
-        <button onClick={this.minus}>-</button>
-      </div>
-    )
-  }
-  plus = () => {
-    this.setState(state => {
-      return {
-        item: state.item + 1
-      }
-    })
-  }
-
-  minus = () => {
-    this.setState(state => {
-      return {
-        item: state.item - 1
-      }
-    })
-  }
+// 함수형 컴포넌트
+const App = () => {
+  const name = useInput("KimBro");
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <input
+        placeholder="Name"
+        //value={name.value}
+        //onChange={name.onChange}
+        {...name}
+      />
+    </div>
+  );
 }
 
 export default App;
