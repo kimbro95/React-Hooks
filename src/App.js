38712e4b010487d98ec69f7b2cc34804efc26963
bro <gudgudgud714@naver.com>
2022-05-26 16:05:55 +0900

    2.0 Introduction to useEffect

--- src/App.js
+++ src/App.js
@@ -1,37 +1,19 @@
-import React, { useState } from "react";
-
-const content = [
-  {
-    tab: "Section 1",
-    content: "Content of the Section 1"
-  },
-  {
-    tab: "Section 2",
-    content: "Content of the Section 2"
-  }
-];
-
-const useTabs = (initialTab, allTabs) => {
-  const [index, setIndex] = useState(initialTab);
-  if (!allTabs || !Array.isArray(allTabs)) return;
-  return {
-    item: allTabs[index],
-    changeItem: setIndex
-  };
-};
+import React, { useEffect, useState } from "react";
 
 const App = () => {
-  // const tab = useTabs(0, content);
-  const { item, changeItem } = useTabs(0, content);
+  const hello = () => console.log("hello !!!");
+  const [number, setNumber] = useState(0);
+  const [number2, setNumber2] = useState(0);
+
+  useEffect(() => {
+    hello();
+  }, []);
 
   return (
     <div className="App">
-      {content.map((section, idx) => (
-        <button key={idx} onClick={() => changeItem(idx)}>{section.tab}</button>
-      ))}
-      <div>
-        {item.content}
-      </div>
+      <div>Hi</div>
+      <button onClick={() => setNumber(number + 1)}>{number}</button>
+      <button onClick={() => setNumber2(number2 + 1)}>{number2}</button>
     </div>
   );
 }
