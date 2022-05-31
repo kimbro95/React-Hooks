c875e7a3066d288d980d54dbb6bc96676c0155ec
bro <gudgudgud714@naver.com>
2022-05-31 14:12:27 +0900

    2.5 useFadeIn & useNetwork

--- src/App.js
+++ src/App.js
@@ -1,27 +1,60 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
-const useBeforeLeave = (onBefore) => {
-  const handle = (event) => {
-    const { clientY } = event;
-    if (clientY <= 0) {
-      onBefore();
+// useFadeIn
+// const useFadeIn = (duration = 1, delay = 0) => {
+//   const element = useRef();
+//   useEffect(() => {
+//     if (typeof duration !== "number" || typeof delay !== "number") return;
+//     if (element.current) {
+//       const { current } = element;
+//       current.style.transition = `opacity ${duration}s ease-in-out ${delay}s`;
+//       current.style.opacity = 1;
+//     }
+//   }, [])
+//   return { ref: element, style: { opacity: 0 } };
+// }
+
+// const App = () => {
+//   const fadeInH1 = useFadeIn(1, 2);
+//   const fadeInP = useFadeIn(3, 5);
+//   return (
+//     <div className="App">
+//       <h1 {...fadeInH1}>Hi</h1>
+//       <p {...fadeInP}>P tag</p>
+//     </div>
+//   );
+// };
+
+
+// useNetwork
+const useNetwork = (onChange) => {
+  const [status, setStatus] = useState(navigator.onLine);
+  const handleChange = () => {
+    if (typeof onChange === "function") {
+      onChange(navigator.onLine);
     }
-  }
+    setStatus(navigator.onLine);
+  };
+
   useEffect(() => {
-    if (typeof onBefore !== "function") return;
-    document.addEventListener("mouseleave", handle);
+    window.addEventListener("online", handleChange);
+    window.addEventListener("offline", handleChange);
     return () => {
-      document.removeEventListener("mouseleave", handle);
+      window.removeEventListener("online", handleChange);
+      window.removeEventListener("offline", handleChange);
     }
   }, []);
+  return status;
 }
 
 const App = () => {
-  const begForLife = () => console.log("pls dont leave");
-  useBeforeLeave(begForLife);
+  const handleNewtworkChange = (online) => {
+    console.log(online ? "Online" : "Offline")
+  };
+  const onLine = useNetwork(handleNewtworkChange);
   return (
     <div className="App">
-      <h1>Hi</h1>
+      <h1>{onLine ? "Online" : "Offline"}</h1>
     </div>
   );
 };
