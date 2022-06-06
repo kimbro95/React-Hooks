d3bfc658200a7fae0c20776de2554df0b0eb56c1
l995lol4 <gudgudgud714@naver.com>
2022-06-06 17:58:39 +0900

    2.7 useNotification

--- src/App.js
+++ src/App.js
@@ -1,68 +1,31 @@
-import { isElementType } from "@testing-library/user-event/dist/utils";
-import React, { useEffect, useRef, useState } from "react";
+import React from "react";
 
-// useScroll 
-// const useScroll  = () => {
-//   const [state, setState ] = useState({
-//     x: 0,
-//     y: 0
-//   });
-//   const onScroll = () => {
-//     setState({
-//       y: window.pageYOffset,
-//       x: window.pageYOffset
-//     });
-//   }
-//   useEffect(() => {
-//     window.addEventListener("scroll", onScroll);
-//     return () => window.removeEventListener("scroll", onScroll);
-//   }, [])
-//   return state;
-// }
-
-// const App = () => {
-//   const { y } = useScroll();
-
-//   return (
-//     <div className="App" style={{ height: "1000vh"}}>
-//       <h1 style={{position: "fixed", color : y > 100 ? "red" : "blue"}}>hi</h1>
-//     </div>
-//   );
-// };
-
-// useFullscreen
-const useFullscreen = (callback) => {
-  const element = useRef();
-  const triggerFull = () => {
-    if(element.current){
-      element.current.requestFullscreen();
-      if(callback && typeof callback === "function"){
-        callback(true);
-      }
+// useNotification
+const useNotification = (title, options) => {
+  if(!("Notification" in window)){
+    return;
+  }
+  const fireNotif = () => {
+    if(Notification.permission !== "granted"){
+      Notification.requestPermission().then(permission => {
+        if(permission === "granted"){
+          new Notification(title, options);
+        } else {
+          return;
+        }
+      });
+    } else {
+      new Notification(title, options);
     }
   }
-  const exitFull = () => {
-    document.exitFullscreen();
-    if(callback && typeof callback === "function"){
-      callback(false);
-    }
-  }
-  return {element, triggerFull, exitFull};
-}
+  return fireNotif;
+};
 
 const App = () => {
-  const onFull = (isFull) => {
-    console.log(isFull ? "Full" : "Not Full");
-  }
-  const {element, triggerFull, exitFull} = useFullscreen(onFull);
-
+  const triggerNotifi = useNotification("Hello", {body : "this is body"});
   return (
     <div className="App">
-      <div ref={element}>
-        <img src="https://i.ibb.co/R6RwNxx/grape.jpg" alt="grape" width="250" />
-        <button onClick={triggerFull}>Full Screen</button>
-        <button onClick={exitFull}>Exit Screen</button>
-      </div>
+      <button onClick={triggerNotifi}>Hi</button>
     </div>
   );
 };
